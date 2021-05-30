9cde0509fe6019682f01d269e09c32c079685e21
ebisLab <ebaiden@techie.com>
2021-05-29 21:12:05 -0400

    divs set up

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Main() {
+    return (
+        <div style={{display:"inline-flex", width:"100%"}}>
+            <div className="mangopulp" style={{width:"33.3%", height:"100vh"}}></div>
+            <div className="navyleaf" style={{width:"33.3%", height:"100vh"}}></div>
+            <div className="palewood2" style={{width:"33.3%", height:"100vh"}}></div> 
+
+            
+        </div>
+    )
+}
