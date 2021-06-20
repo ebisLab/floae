115e157fddb03c807f106d3c644cb60d78e6055f
ebisLab <ebaiden@techie.com>
2021-06-20 07:18:53 -0500

    retracted shopping bag

--- src/components/Main.js
+++ src/components/Main.js
@@ -91,7 +91,7 @@ const addIntoCart=(e,item)=>{
                 </ul>
                 <ul className="menu_list" style={{display:"inline-flex", listStyle:"none", float:"right", color:'#ffecc8'}}>
                 <ul className="menu_list" style={{display:"inline-flex", listStyle:"none", float:"right", color:"wheat"}}>
-                <li>
+                {/* <li>
                     <Link to="/cart">
                         <i 
                         className="pe-7s-shopbag" 
@@ -109,7 +109,7 @@ const addIntoCart=(e,item)=>{
                             borderRadius:"50%"
                             }}>{checkout.length>0 ? localCart.length: checkout.length }</span>
                     </Link>
-                </li>
+                </li> */}
 
                     <li><Link to="/cart"><i className="pe-7s-cart" style={{fontSize:"1.7em", fontWeight:"600"}}></i><span className={checkout.length>0?"stuff_cart":""} style={{color:"tomato", fontWeight: 700, position:"relative", top:'-20px', left:'-14px', borderRadius:"50%"}}>{checkout.length}</span></Link></li>
                     <li><i className="pe-7s-user" style={{fontSize:"1.7em", fontWeight:"600"}}></i></li>
